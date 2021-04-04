1d7ab719c47f0cd89fb640f3323eb6ae851a774b
aldhykohar <aldhykohar@stimednp.ac.id>
2021-04-04 19:11:12 +0800

    binds anotation

--- app/src/main/java/com/aldhykohar/daggerhilt/MainActivity.kt
+++ app/src/main/java/com/aldhykohar/daggerhilt/MainActivity.kt
@@ -3,6 +3,7 @@ package com.aldhykohar.daggerhilt
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.aldhykohar.daggerhilt.demo.Car
+import com.aldhykohar.daggerhilt.demo.Main
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -13,9 +14,13 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var car: Car
 
+    @Inject
+    lateinit var main: Main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         car.getCar()
+        main.getName()
     }
 }
